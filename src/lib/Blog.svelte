5ed600d65aa53b78391b7b0409b4212c31951b07
Nwankwo Onyedika Benson <nwankwobenson29@gmail.com>
2024-08-10 14:44:18 +0100

    Created the routing system between pages

--- src/lib/Blog.svelte
+++ src/lib/Blog.svelte
@@ -1,6 +1,12 @@
 <script>
+    import PocketBase from "pocketbase";
+
+    const pb = new PocketBase("http://127.0.0.1:8090");
+    
     
 </script>
 <main>
     <h1>Blog Site</h1>
+
+    
 </main>
